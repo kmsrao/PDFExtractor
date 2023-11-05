822882f07e9d8526e1c46ccb692e47f78432d57c
kmsrao <kmsrao@gmail.com>
2023-11-05 22:47:27 +0530

    Add files via upload

---
+++ PdfDataExtractor/MainWindow.xaml.cs
@@ -0,0 +1,269 @@
+﻿using CefSharp.Internals;
+using PdfDataExtractor.PdfUtils;
+using System.Drawing;
+using System.Text;
+using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig.Core;
+using UglyToad.PdfPig.Logging;
+
+namespace PdfDataExtractor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //constants
+        int BORDERMARGIN = 30;
+
+
+        //Localvariables.
+        string PDFFileName = string.Empty;
+        double scaleX = 1;
+        double scaleY = 1;
+        bool drawMode = true;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void UpdateUI()
+        {
+            double FullWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+            double FullHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+            this.Width = FullWidth;
+            this.Height = FullHeight;
+
+            //Command and Control GroupBox Position Layout. Group box line has the GRP Margins.
+            //Compensate the same while adjusting the Controls.MK
+            grp_commandAndControl.Width = (this.Width / 2) - BORDERMARGIN;
+            grp_commandAndControl.Height = this.Height - (BORDERMARGIN * 4);
+
+            Canvas.SetTop(grp_commandAndControl, BORDERMARGIN);
+            Canvas.SetLeft(grp_commandAndControl, BORDERMARGIN);
+
+
+            //Pdf Viwer GroupBox Position Layout. Group box line has the GRP Margins.
+            //Compensate the same while adjusting the Controls.MK
+            grp_PdfViewer.Width = (this.Width / 2) - BORDERMARGIN;
+            grp_PdfViewer.Height = this.Height - (BORDERMARGIN * 4);
+
+            Canvas.SetTop(grp_PdfViewer, BORDERMARGIN);
+            Canvas.SetLeft(grp_PdfViewer, this.Width / 2);
+
+
+            //Adjust Controls for PDfViewer.
+            this.pdf_canvas.Width = grp_PdfViewer.Width;
+            this.pdf_canvas.Height = grp_PdfViewer.Height;
+
+            this.pdfviewer.Width = pdf_canvas.Width;
+            this.pdfviewer.Height = pdf_canvas.Height;
+
+
+        }
+
+        private void can_CommandAndControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+            UpdateUI();
+        }
+
+        private void btn_LoadPdf_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.Filter = "Pdf |*.pdf";
+
+            if (dialog.ShowDialog() == true)
+            {
+                pdfviewer.Address = dialog.FileName;
+                PDFFileName = dialog.FileName;
+                PdfWrapper pdfWrapper = new PdfWrapper();
+                scaleX = pdfWrapper.GetXdimensions(PDFFileName) / pdf_canvas.Width;
+                scaleY = pdfWrapper.GetYdimensions(PDFFileName) / (pdf_canvas.Height - 120);
+
+                AdjustLayoutForPdf(PDFFileName);
+
+
+            }
+        }
+
+        private void AdjustLayoutForPdf(string PDFFileName)
+        {
+
+            PdfWrapper pdfWrapper = new PdfWrapper();
+            double PDFWidth = pdfWrapper.GetXdimensions(PDFFileName);
+            double PDFHeight = pdfWrapper.GetYdimensions(PDFFileName);
+
+            double PDFAspectRatioWidth = pdfviewer.Width / PDFWidth;
+            double PDFAspectRatioHeight = pdfviewer.Height / PDFHeight;
+
+            double ScaleFactor = 1;
+            if (PDFAspectRatioWidth < PDFAspectRatioHeight)
+            {
+                //Set the Height : KMSR
+                pdfviewer.Height = pdfviewer.Width * PDFHeight / PDFWidth;
+                pdfdraw_canvas.Height = pdfviewer.Height;
+
+
+            }
+            else
+            {
+                //Set the Width  : KMSR
+                pdfviewer.Width = pdfviewer.Height * PDFWidth / PDFHeight;
+                pdfdraw_canvas.Width = pdfviewer.Width;
+            }
+
+
+            //Set the dimensions of the PDF Container as per the PDF Size  : KMSR
+            pdfdraw_canvas.Width = pdfviewer.Width;
+            pdfdraw_canvas.Height = pdfviewer.Height;
+            pdfdraw_canvas.Margin = pdfviewer.Margin;
+
+
+            //Scale Factor to convert from control locatin to PDf Location : KMSR
+            scaleX = PDFWidth / pdfdraw_canvas.Width;
+            scaleY = PDFHeight / (pdfdraw_canvas.Height - 60);
+
+
+
+        }
+
+        private void btn_enabledraw_Click(object sender, RoutedEventArgs e)
+        {
+            if (drawMode)
+            {
+                drawMode = false;
+                btn_enabledraw.Content = "Enable Draw";
+                pdfdraw_canvas.Visibility = Visibility.Hidden;
+
+            }
+            else
+            {
+                drawMode = true;
+                btn_enabledraw.Content = "Disable Draw";
+                pdfdraw_canvas.Visibility = Visibility.Visible;
+            }
+        }
+
+        private System.Windows.Point mouseLeftDownPoint;
+        private void pdfdraw_canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!pdfdraw_canvas.IsMouseCaptured && drawMode)
+            {
+                mouseLeftDownPoint = e.GetPosition(pdfdraw_canvas);
+                pdfdraw_canvas.CaptureMouse();
+            }
+        }
+
+        private System.Windows.Shapes.Rectangle rubberBand = null;
+        private void pdfdraw_canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            System.Windows.Point currentPoint = e.GetPosition(pdfdraw_canvas);
+
+            currentPoint = new System.Windows.Point(currentPoint.X, currentPoint.Y);
+
+            if (pdfdraw_canvas.IsMouseCaptured)
+            {
+
+
+                if (rubberBand == null)
+                {
+                    rubberBand = new System.Windows.Shapes.Rectangle();
+                    rubberBand.Stroke = new SolidColorBrush(Colors.Red);
+                    pdfdraw_canvas.Children.Add(rubberBand);
+                }
+
+                double width = Math.Abs(mouseLeftDownPoint.X - currentPoint.X);
+                double height = Math.Abs(mouseLeftDownPoint.Y - currentPoint.Y);
+                double left = Math.Min(mouseLeftDownPoint.X, currentPoint.X);
+                double top = Math.Min(mouseLeftDownPoint.Y, currentPoint.Y);
+
+                rubberBand.Width = width;
+                rubberBand.Height = height;
+
+                rectangle.Width = (int)rubberBand.Width;
+                rectangle.Height = (int)rubberBand.Height;
+                rectangle.X = (int)left;
+                rectangle.Y = (int)top;
+
+                Canvas.SetLeft(rubberBand, left);
+                Canvas.SetTop(rubberBand, top);
+            }
+        }
+
+        System.Drawing.Rectangle rectangle = new System.Drawing.Rectangle();
+        private void pdfdraw_canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (pdfdraw_canvas.IsMouseCaptured && rubberBand != null)
+            {
+                pdfdraw_canvas.ReleaseMouseCapture();
+
+                pdfdraw_canvas.Children.Clear();
+
+                if (rubberBand != null)
+                {
+                    rectangle.Width = (int)rubberBand.Width;
+                    rectangle.Height = (int)rubberBand.Height;
+                    rubberBand = null;
+                }
+
+
+            }
+
+            rectangle.Y = (int)(pdfdraw_canvas.Height - rectangle.Y);
+
+
+            rectangle.X = (int)((double)rectangle.X * scaleX);
+            rectangle.Width = (int)((double)rectangle.Width * scaleX);
+            rectangle.Y = (int)((double)rectangle.Y * scaleY);
+            rectangle.Height = (int)((double)rectangle.Height * scaleY);
+
+
+
+
+            PdfWrapper pdfWrapper = new PdfWrapper();
+
+            //Locations are availble. Get the text from PDF from this location.
+
+            if (rb_textselected.IsChecked.Value)
+            {
+                List<string> textsfound = pdfWrapper.GetTextsFromLocation(PDFFileName, new PdfRectangle(rectangle.X, rectangle.Y, rectangle.X + rectangle.Width, rectangle.Y - rectangle.Height));
+                string concatstring = string.Empty;
+
+                foreach (string text in textsfound)
+                {
+
+                    concatstring += text;
+                }
+
+                MessageBox.Show(concatstring);
+            }
+            else
+            {
+
+                img_selectedfromPDF.Source = pdfWrapper.GetAllImageLocationsFromLocation(PDFFileName, new PdfRectangle(rectangle.X, rectangle.Y, rectangle.X + rectangle.Width, rectangle.Y - rectangle.Height));
+
+                //if (Imagefound != string.Empty)
+                //{
+                //    img_selectedfromPDF.Source = new BitmapImage(new Uri(Imagefound));
+
+                //}
+
+                //pdfWrapper.ExtractAllImages(PDFFileName);
+            }
+        }
+    }
+}
---
+++ PdfDataExtractor/PdfUtils/PdfWrapper.cs
@@ -0,0 +1,393 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig.Core;
+using UglyToad.PdfPig.XObjects;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Geometry;
+using System.Windows.Controls;
+using System.Diagnostics;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace PdfDataExtractor.PdfUtils
+{
+    public class PdfWrapper
+    {
+        public void ExtractAllImages(string PdfFile)
+        {
+
+            using (var doc = PdfDocument.Open(PdfFile))
+            {
+                ImageIndex = 0;
+                foreach (var page in doc.GetPages())
+                {
+                    ExtractAllImagesFromPage(page);
+
+
+                }
+            }
+
+        }
+
+        int ImageIndex = 0;
+        private void ExtractAllImagesFromPage(UglyToad.PdfPig.Content.Page page)
+        {
+            
+            foreach (XObjectImage image in page.GetImages())
+            {
+                var type = string.Empty;
+
+                switch (image)
+                {
+                    case XObjectImage ximg:
+                        type = "XObject";
+                        break;
+                    //case InlineImage inline:
+                    //    type = "Inline";
+                    //    break;
+                }
+
+                byte[] pngImage;
+
+                bool extractionSuccessful = image.TryGetPng(out pngImage);
+
+                if (extractionSuccessful)
+                {
+                    File.WriteAllBytes("D:\\Sample" + ImageIndex.ToString() + ".png", pngImage);
+                    ImageIndex++;
+                }
+
+            }
+        }
+
+
+        public List<PdfRectangle> GetAllImagesLocations(string PdfFile)
+        {
+
+            List<PdfRectangle> imagelocations = new List<PdfRectangle>();
+
+            using (var doc = PdfDocument.Open(PdfFile))
+            {
+                foreach (var page in doc.GetPages())
+                {
+                    imagelocations.AddRange(GetAllImageLocationsFromPage(page));
+                }
+            }
+
+            return imagelocations;
+        }
+
+        public BitmapSource GetAllImageLocationsFromLocation(string PdfFile,PdfRectangle location)
+        {
+
+           
+            using (var doc = PdfDocument.Open(PdfFile))
+            {
+                foreach (var page in doc.GetPages())
+                {
+                    return GetAllImageLocationsFromLocation(page, location);
+                }
+            }
+
+            return null;
+        }
+        
+        private List<PdfRectangle> GetAllImageLocationsFromPage(UglyToad.PdfPig.Content.Page page)
+        {
+
+            List<PdfRectangle> imagelocations = new List<PdfRectangle>();
+
+            foreach (var image in page.GetImages())
+            {
+                PdfRectangle rectangle = image.Bounds;
+
+            }
+
+            return imagelocations;
+        }
+
+
+        private BitmapSource  GetAllImageLocationsFromLocation(UglyToad.PdfPig.Content.Page page,PdfRectangle pdfRectangle)
+        {
+            string SamplePNGFile = "D:\\Sample.png";
+
+         
+            if (File.Exists(SamplePNGFile))
+            {
+                File.Delete(SamplePNGFile);
+            }
+
+            List<PdfRectangle> imagelocations = new List<PdfRectangle>();
+
+
+            foreach (var image in page.GetImages())
+            {
+                PdfRectangle rectangle = image.Bounds;
+
+                if(pdfRectangle.Contains(rectangle.Centroid))
+                {
+                    return TryGetImage(image);
+
+                    //byte[] pngImage;
+
+                    
+                    //bool extractionSuccessful = image.TryGetPng(out pngImage);
+
+                    //if (extractionSuccessful)
+                    //{
+                    //    File.WriteAllBytes(SamplePNGFile, pngImage);
+
+
+
+                    //    return SamplePNGFile;
+                    //}
+                }
+
+            }
+            return null;
+            
+        }
+
+        public List<string> GetTextsFromLocation(string PdfFile, PdfRectangle rectangle)
+        {
+
+            List<string> wordsinRect = new List<string>();
+
+            using (var doc = PdfDocument.Open(PdfFile))
+            {
+                foreach (var page in doc.GetPages())
+                {
+
+                    wordsinRect.AddRange(GetTextFromLocationPage(page, rectangle));
+                    break;
+
+                }
+            }
+
+            return wordsinRect;
+        }
+
+
+
+        public List<string> GetTextFromLocationPage(UglyToad.PdfPig.Content.Page page, PdfRectangle rectangle)
+        {
+
+            List<string> TextinLocation = new List<string>();
+
+            List<Word> wordsinPage = page.GetWords().ToList();
+
+
+            foreach (var Word in wordsinPage)
+            {
+                if (rectangle.Contains(Word.BoundingBox.Centroid))
+                {
+                    TextinLocation.Add(Word.Text);
+                }
+
+            }
+            return TextinLocation;
+        }
+
+        public List<string> GetTextFromLocationPage(string PdfFile, int pageNumber, PdfRectangle rectangle)
+        {
+            List<string> TextinLocation = new List<string>();
+
+            using (var doc = PdfDocument.Open(PdfFile))
+            {
+                var page = doc.GetPages().ToList()[pageNumber];
+
+                List<Word> wordsinPage = page.GetWords().ToList();
+
+                foreach (var Word in wordsinPage)
+                {
+                    if (rectangle.Contains(Word.BoundingBox.Centroid))
+                    {
+                        TextinLocation.Add(Word.Text);
+                    }
+
+
+                }
+            }
+            return TextinLocation;
+        }
+
+
+        public List<string> GetAllTexts(string PdfFile)
+        {
+
+            List<string> wordsinRect = new List<string>();
+
+            using (var doc = PdfDocument.Open(PdfFile))
+            {
+                foreach (var page in doc.GetPages())
+                {
+
+                    wordsinRect.AddRange(GetTextFromPage(page));
+                    break;
+
+                }
+            }
+
+            return wordsinRect;
+        }
+        private List<string> GetTextFromPage(UglyToad.PdfPig.Content.Page page)
+        {
+
+            List<string> TextinLocation = new List<string>();
+
+            List<Word> wordsinPage = page.GetWords().ToList();
+
+            foreach (var Word in wordsinPage)
+            {
+                TextinLocation.Add(Word.Text);
+            }
+
+            return TextinLocation;
+        }
+
+        public List<Word> GetInteractiveAllTexts(string PdfFile)
+        {
+
+            List<Word> wordsinRect = new List<Word>();
+
+            using (var doc = PdfDocument.Open(PdfFile))
+            {
+                foreach (var page in doc.GetPages())
+                {
+
+                    wordsinRect.AddRange(GetInteractiveTextFromPage(page));
+                    break;
+
+                }
+            }
+
+            return wordsinRect;
+        }
+
+        public double GetXdimensions(string PdfFile)
+        {
+
+            using (var doc = PdfDocument.Open(PdfFile))
+            {
+                foreach (var page in doc.GetPages())
+                {
+
+                    return page.Width;
+
+                }
+            }
+
+            return 0;
+        }
+
+        public double GetYdimensions(string PdfFile)
+        {
+
+            using (var doc = PdfDocument.Open(PdfFile))
+            {
+                foreach (var page in doc.GetPages())
+                {
+
+                    return page.Height;
+
+                }
+            }
+
+            return 0;
+        }
+        private List<Word> GetInteractiveTextFromPage(UglyToad.PdfPig.Content.Page page)
+        {
+
+            StreamWriter sw = new StreamWriter("D:\\position.txt");
+
+
+            List<Word> wordsinPage = page.GetWords().ToList();
+
+
+
+            foreach (var Word in wordsinPage)
+            {
+
+                sw.WriteLine(Word.Text + ":: X - " + Word.BoundingBox.TopLeft.X.ToString() + " :: Y- " + Word.BoundingBox.TopLeft.Y.ToString());
+
+                //if (Word.BoundingBox.Contains(new PdfPoint(location.X+location.Width/2 ,location.Y+ location.Height/2)))
+                //{
+                //    TextinLocation.Add(Word.Text);
+                //}
+
+            }
+            sw.Close();
+
+
+            return page.GetWords().ToList();
+
+        }
+
+        private static BitmapSource TryGetImage(IPdfImage image)
+        {
+            BitmapSource bmp;
+            byte[] bytes;
+            if (image.TryGetPng(out bytes))
+            {
+                bmp = (BitmapSource)new ImageSourceConverter().ConvertFrom(bytes);
+
+            }
+            else
+            {
+                IReadOnlyList<byte> iroBytes;
+                if (image.TryGetBytes(out iroBytes))
+                {
+                    bmp = (BitmapSource)new ImageSourceConverter().ConvertFrom(bytes);
+                }
+                else
+                {
+                    var rawB = image.RawBytes.ToArray<Byte>();
+
+                    
+                    using (var ms = new MemoryStream(rawB))
+                    {
+                       var nbmp = new BitmapImage();
+                        nbmp.BeginInit();
+                        nbmp.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                        nbmp.CacheOption = BitmapCacheOption.OnLoad;
+                        nbmp.StreamSource = ms;
+                        nbmp.EndInit();
+
+                        return nbmp;
+
+
+                    }
+                   
+                   
+                }
+            }
+            return bmp;
+        }
+
+        //public static BitmapSource ConvertBmpToBmpSource(BitmapImage bitmap)
+        //{
+        //    var bitmapData = bitmap.LockBits(
+        //        new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+        //        System.Drawing.Imaging.ImageLockMode.ReadOnly, bitmap.PixelFormat);
+
+        //    var bitmapSource = BitmapSource.Create(
+        //        bitmapData.Width, bitmapData.Height,
+        //        bitmap.HorizontalResolution, bitmap.VerticalResolution,
+        //        PixelFormats.Bgr24, null,
+        //        bitmapData.Scan0, bitmapData.Stride * bitmapData.Height, bitmapData.Stride);
+
+        //    bitmap.UnlockBits(bitmapData);
+
+        //    return bitmapSource;
+        //}
+
+
+
+    }
+
+}
